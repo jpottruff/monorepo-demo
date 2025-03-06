b31c4534a6c2b81a9c58a4c89ed33cc46f0bc840
Jeff Pottruff <jeffpottruff@gmail.com>
2025-03-05 23:34:36 -0500

    linting - install eslint-config-prettier and eslint-plugin-prettier. add script. add config mods

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
@@ -8,7 +10,12 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
+    plugins: {
+      prettier: prettierPlugin,
+    },
     rules: {
+      ...prettier.rules,
+      'prettier/prettier': 'error',
       'no-unused-vars': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
     },
