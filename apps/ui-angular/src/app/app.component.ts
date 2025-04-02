b0b6f68ea3b252a07efe61260afed2038f39d0bd
Jeff Pottruff <jeffpottruff@gmail.com>
2025-04-02 15:54:21 -0400

    1 - add router outlet to app

--- apps/ui-angular/src/app/app.component.ts
+++ apps/ui-angular/src/app/app.component.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 import { FooterComponent } from '../layout/footer/footer.component';
 import { HeaderComponent } from '../layout/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, ButtonModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'ui-angular';
-}
+export class AppComponent {}
