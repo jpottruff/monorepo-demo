0de16bdecc1b5fda3a59d968a31ee1275681b1d6
Jeff Pottruff <jeffpottruff@gmail.com>
2025-04-02 15:53:52 -0400

    1 - set up route for home. set up lazy loaded routes for forms

--- apps/ui-angular/src/app/app.routes.ts
+++ apps/ui-angular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('../features/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'forms',
+    loadChildren: () =>
+      import('../features/forms/forms.routes').then((m) => m.FORMS_ROUTES),
+  },
+  { path: '**', redirectTo: '' },
+];
--- apps/ui-angular/src/layout/header/header.component.ts
+++ apps/ui-angular/src/layout/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonModule],
+  imports: [RouterLink, ButtonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
