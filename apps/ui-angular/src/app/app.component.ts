ccf761d760b186cfaac14818955d63307ce0b340
Jeff Pottruff <jeffpottruff@gmail.com>
2025-04-02 15:14:48 -0400

    1 - add header component and hook up

--- apps/ui-angular/src/app/app.component.ts
+++ apps/ui-angular/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from '../layout/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonModule],
+  imports: [RouterOutlet, HeaderComponent, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ apps/ui-angular/src/layout/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-header',
+  imports: [ButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
