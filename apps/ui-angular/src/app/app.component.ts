05845e643bba6608fdafa04eb1d41a3ba3abce2f
Jeff Pottruff <jeffpottruff@gmail.com>
2025-04-02 15:18:02 -0400

    1 - add footer component and hook up

--- apps/ui-angular/src/app/app.component.ts
+++ apps/ui-angular/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { FooterComponent } from '../layout/footer/footer.component';
 import { HeaderComponent } from '../layout/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ButtonModule],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
