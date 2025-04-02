8209bd696dc7b74ff6e475b6ef8859d8c2546a4e
Jeff Pottruff <jeffpottruff@gmail.com>
2025-04-02 15:34:39 -0400

    1 - add boiler plate for 3 page components

---
+++ apps/ui-angular/src/features/forms/template-forms/template-forms.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-template-forms',
+  imports: [],
+  templateUrl: './template-forms.component.html',
+  styleUrl: './template-forms.component.css',
+})
+export class TemplateFormsComponent {}
