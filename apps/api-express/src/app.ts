230f6910be275d7bb9dc5d3ba06a62a7e0509222
Jeff Pottruff <jeffpottruff@gmail.com>
2025-04-03 14:17:55 -0400

    3 - handle cors error in api

--- apps/api-express/src/app.ts
+++ apps/api-express/src/app.ts
@@ -1,7 +1,24 @@
+import cors from 'cors';
 import express, { Application } from 'express';
 import healthCheckRoutes from './routes/healthcheck.routes';
 
 const app: Application = express();
+
+// TODO - this should be tightened up
+app.use(
+  cors({
+    origin: '*',
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+  })
+);
+
 app.use(express.json());
 
 // Routes
