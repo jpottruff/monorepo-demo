69abc2684e77cc87670dcef5fcb5ff5eafa164af
Jeff Pottruff <jeffpottruff@gmail.com>
2025-04-03 15:37:51 -0400

    3 - install types in api and modify response

--- apps/api-express/src/controllers/healthcheck.controller.ts
+++ apps/api-express/src/controllers/healthcheck.controller.ts
@@ -1,11 +1,15 @@
+import { HealthCheckData, HealthStatus } from '@monorepo-demo/types';
 import { NextFunction, Request, Response } from 'express';
 
 class HealthCheckController {
   static getHealthCheck(_req: Request, res: Response, _next: NextFunction) {
-    const serverStatus = 'up';
-    // TODO Replace with actual MongoDB connection check
-    const mongoStatus = 'disconnected';
-    res.json({ server: serverStatus, database: mongoStatus });
+    // TODO - implement mongoDB connection check
+    const healthCheckResponse: HealthCheckData = {
+      application: 'api-express',
+      serverStatus: HealthStatus.ONLINE,
+      mongoStatus: HealthStatus.DISCONNECTED,
+    };
+    res.json(healthCheckResponse);
   }
 }
 
