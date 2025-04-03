56b22b709f784ecc7f8ca7f5df21edf4a1f10714
Jeff Pottruff <jeffpottruff@gmail.com>
2025-04-03 15:39:57 -0400

    3 - remove stub data in ui

--- apps/ui-angular/src/features/health-check/health-check.service.ts
+++ apps/ui-angular/src/features/health-check/health-check.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { HealthCheckData, HealthStatus } from '@monorepo-demo/types';
+import { HealthCheckData } from '@monorepo-demo/types';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable({
@@ -14,29 +14,8 @@ export class HealthCheckService {
     const endpoint = '/api/healthcheck';
 
     const data = await lastValueFrom(
-      this.httpClient.get<HealthCheckData[]>(`${baseUrl}${endpoint}`)
+      this.httpClient.get<HealthCheckData>(`${baseUrl}${endpoint}`)
     );
-    console.log(data);
-    return stubData();
+    return [data];
   }
-}
-
-function stubData(): HealthCheckData[] {
-  return [
-    {
-      application: 'Application 1',
-      serverStatus: HealthStatus.ONLINE,
-      mongoStatus: HealthStatus.CONNECTED,
-    },
-    {
-      application: 'Application 2',
-      serverStatus: HealthStatus.OFFLINE,
-      mongoStatus: HealthStatus.DISCONNECTED,
-    },
-    {
-      application: 'Application 3',
-      serverStatus: HealthStatus.ONLINE,
-      mongoStatus: HealthStatus.CONNECTED,
-    },
-  ];
 }
