b04cecda6fa91a58009cfbdc7671b593f78ea1b7
Jeff Pottruff <jeffpottruff@gmail.com>
2025-04-05 09:16:00 -0400

    6 - add basic logger

---
+++ libs/logger/src/logger.ts
@@ -0,0 +1,33 @@
+import { SeqTransport } from '@datalust/winston-seq';
+import dotenv from 'dotenv';
+import path from 'path';
+import winston from 'winston';
+
+const dotenvPath = path.resolve(__dirname, '../../../.env');
+dotenv.config({ path: dotenvPath });
+
+if (!process.env.SEQ_SERVER_URL) {
+  console.error(
+    'Missing required environment variables for Seq logging: SEQ_SERVER_URL'
+  );
+  process.exit(1);
+}
+
+const logger = winston.createLogger({
+  transports: [
+    new SeqTransport({
+      serverUrl: process.env.SEQ_SERVER_URL,
+      // apiKey: process.env.SEQ_API_KEY, // TODO: Add API key if needed
+      onError: (e) => {
+        console.error(e);
+      },
+      handleExceptions: true,
+      handleRejections: true,
+    }),
+  ],
+  exitOnError: false,
+});
+
+logger.info('Logger initialized at {url}', { url: process.env.SEQ_SERVER_URL });
+
+export default logger;
